c53b0d77c04389d6db09a94452eb7979a568db71
Dimitri PACINI <dpacini@imv-technologies.com>
2021-07-12 14:46:25 +0200

    Add project files.

---
+++ DB_MFR/character.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[character]
+(
+	[Id_character] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [user_id] INT NOT NULL, 
+    [character_name] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_character_user] FOREIGN KEY ([user_id]) REFERENCES [user]([Id_user])
+)
---
+++ DB_MFR/user.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[Id_user] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [user_name] VARCHAR(50) NOT NULL, 
+    [creation_date] SMALLDATETIME NOT NULL, 
+    [last_login_date] SMALLDATETIME NULL, 
+    [must_change_password] BIT NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_user_user_name] ON [dbo].[user] ([user_name])
