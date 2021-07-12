c638514a6c64512eaad689df78cd57257835ad81
Neopatch <neopatch@gmail.com>
2021-07-12 15:27:10 +0200

    test commit

---
+++ DB_MFR/character_type.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[character_type]
+(
+	[Id_character_type] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [character_type_name] VARCHAR(50) NOT NULL
+)
+
+GO
+
+CREATE INDEX [IX_character_type_character_type_name] ON [dbo].[character_type] ([character_type_name])
